396fd13a5aaf76ed281a0d47258997c1f89ad503
subendra mandal <subendra483@gmail.com>
2023-01-15 17:45:04 +0545

    This is slider with api image with custom animation in with css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "animate.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 function App() {
+  const [item, setItem] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get("https://dummyjson.com/products");
+      setItem(res.data.products);
+      console.log(res.data.products);
+    };
+
+    fetchData();
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Slider className="slider_style" {...settings}>
+        {item.map((pro) => (
+          <>
+            <div className="image_style">
+              <img key={pro.id} src={pro.thumbnail} alt={pro.title} />
+            </div>
+
+            <h1>{pro.title}</h1>
+          </>
+        ))}
+      </Slider>
+    </>
   );
 }
 
